2df1c45d2d360acba2431ff6c1df69c4ad8a24d2
Noah Mogil <nmogil@twilio.com>
2025-02-24 14:03:54 -0500

    update front end

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -7,6 +7,12 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    cors: true,
+    hmr: {
+      host: "fe4e-2603-7000-8c00-136e-91a3-1c85-c2a3-f816.ngrok-free.app",
+      protocol: "https",
+      clientPort: 443
+    }
   },
   plugins: [
     react(),
